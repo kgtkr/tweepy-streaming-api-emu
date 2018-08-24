69fa3c60e921134ebf8ce7e4757642b3e58b63a4
kgtkr <kgtkr.jp@gmail.com>
2018-08-24 18:34:33 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='tweepy-streaming-api-emu',
+    version='0.1',
+    description='emulate streaming api',
+    url='https://github.com/kgtkr/tweepy-streaming-api-emu',
+    author='kgtkr',
+    author_email='kgtkr.jp@gmail.com',
+    license='MIT',
+    keywords='twitter',
+    packages=[
+    ],
+    install_requires=["tweepy>=3.6.0"],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
